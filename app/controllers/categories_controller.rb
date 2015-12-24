2482ae61b7a93f146003a493a712bf0f220996af
Gilbert Yoder <yodergilbert1@gmail.com>
2015-12-23 21:13:39 -0500

    Added form for New Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,22 @@
 class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(cat_params)
+    if @category.save
+      redirect_to todos_path, notice: "Category Created"
+    else
+      redirect_to :back, notice: "Error Creating Category"
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:category).permit!
+  end
+
 end
