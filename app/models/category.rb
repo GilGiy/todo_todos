e9b8edc50d62fcfd3c540d357bec37a1a3047825
Gilbert Yoder <yodergilbert1@gmail.com>
2016-01-03 15:50:23 -0500

    added validation for creation, tweaked layouts, still have new page, no gui way to get there

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :todos
   belongs_to :user
+
+  validates_presence_of :user_id, :title, :color
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,6 @@
 class Todo < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+
+  validates_presence_of :user_id, :category_id, :title, :body, :due_date
 end
